d53dcc1e91d3d09d81daf4f2e357f8bb02daf633
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-25 22:14:32 +0800

    Add files via upload

--- game.h
+++ game.h
@@ -2,11 +2,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-#define ROW 3
-#define COL 3
+#define ROW 5
+#define COL 5
 void Initialboard(char board[ROW][COL], int row, int col);
 void Displayboard(char board[ROW][COL], int row, int col);
 void playeremove(char board[ROW][COL], int row, int col);
-void computermove(char board[ROW][COL],int  row,int col);
+void computermove(char board[ROW][COL], int  row, int col);
 char Iswin(char board[ROW][COL], int  row, int col);
 char Isfull(char board[ROW][COL], int row, int col);
