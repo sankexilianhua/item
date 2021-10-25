2556dcd38bc3aea2b5c466d767f63b998586b3e4
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-25 22:12:44 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,12 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#define ROW 3
+#define COL 3
+void Initialboard(char board[ROW][COL], int row, int col);
+void Displayboard(char board[ROW][COL], int row, int col);
+void playeremove(char board[ROW][COL], int row, int col);
+void computermove(char board[ROW][COL],int  row,int col);
+char Iswin(char board[ROW][COL], int  row, int col);
+char Isfull(char board[ROW][COL], int row, int col);
